333f993359fa54f19c467f91d0b57a12c7236a8f
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2016-10-09 13:10:54 -0600

    added initial content for Java 8

---
+++ advanced_java/src/main/java/com/xyzcorp/Box.java
@@ -0,0 +1,42 @@
+package com.xyzcorp;
+
+public class Box<A> {
+    private A contents;
+
+    public Box() {
+    }
+
+    public Box(A contents) {
+        this.contents = contents;
+    }
+
+    public void setContents(A contents) {
+        this.contents = contents;
+    }
+
+    public A getContents() {
+        return contents;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public boolean equals(Object o) {
+        if (!(o instanceof Box)) return false;
+        else return equals((Box<A>) o);
+    }
+
+    public boolean equals(Box<A> box) {
+        return this.contents.equals(box.contents);
+    }
+
+    @Override
+    public String toString() {
+        return "Box[" + contents.toString() + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        //Store constant non-zero value and store onto contents `result`
+        return 449 * contents.hashCode();
+    }
+}
---
+++ advanced_java/src/main/java/com/xyzcorp/people/SanFranciscan.java
@@ -0,0 +1,10 @@
+package com.xyzcorp.people;
+
+public class SanFranciscan extends NorthernCalifornian {
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("San Franciscan{");
+        sb.append('}');
+        return sb.toString();
+    }
+}
